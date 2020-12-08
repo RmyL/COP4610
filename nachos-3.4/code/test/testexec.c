530985d215d0dc175488214ee1f2bca0057c3493
William Valido <valido.william@gmail.com>
2020-12-07 21:24:25 -0500

    changes

--- nachos-3.4/code/test/testexec.c
+++ nachos-3.4/code/test/testexec.c
@@ -5,6 +5,6 @@ main()
 {
     PrintString("Before calling Exec.\n");
     Exec("../test/vectorsum");
-    PrintString("Returned from Exec.\n"); // Should never return
+    PrintString("Returned from Exec.\n");
     return 0;
 }
